c24aa1c5a6c44bd6c6866d67abedadfdfff40f2d
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2025-02-12 07:53:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,67 +1,67 @@
 console.log("scripts.js running")
-import contrastChanger from "./contrastChanger.js";
-import highlightLink from "./highlightLinks.js";
-import biggerText from "https://nikhilignattius.github.io/biggerText.js";
-import increaseLineHeight from "https://nikhilignattius.github.io/lineHeight.js";
-import saturationChange from "https://nikhilignattius.github.io/saturation.js";
-import cursorChanger from "https://nikhilignattius.github.io/cursor.js";
+// import contrastChanger from "./contrastChanger.js";
+// import highlightLink from "./highlightLinks.js";
+// import biggerText from "https://nikhilignattius.github.io/biggerText.js";
+// import increaseLineHeight from "https://nikhilignattius.github.io/lineHeight.js";
+// import saturationChange from "https://nikhilignattius.github.io/saturation.js";
+// import cursorChanger from "https://nikhilignattius.github.io/cursor.js";
 // import changeAlignment from "./textAlignment.js";
 // import textSpacing from "./textSpacing.js";
 // import changeText from "./legibleFonts.js";
 // import hideImages from "./hideImages.js";
 // import toolTip from "./toolTips.js";
 
-const parentDocument = window.parent.document;
+// const parentDocument = window.parent.document;
 
-document
-  .getElementById("contrastChange")
-  .addEventListener("click", contrastChanger);
+// document
+//   .getElementById("contrastChange")
+//   .addEventListener("click", contrastChanger);
 
-document
-  .getElementById("highlightLinks")
-  .addEventListener("click", highlightLink);
+// document
+//   .getElementById("highlightLinks")
+//   .addEventListener("click", highlightLink);
 
-document.getElementById("biggerText").addEventListener("click", biggerText);
+// document.getElementById("biggerText").addEventListener("click", biggerText);
 
-document.getElementById("textSpacing").addEventListener("click", textSpacing);
+// document.getElementById("textSpacing").addEventListener("click", textSpacing);
 
-document.getElementById("hideImages").addEventListener("click", hideImages);
+// document.getElementById("hideImages").addEventListener("click", hideImages);
 
-document
-  .getElementById("lineHeight")
-  .addEventListener("click", increaseLineHeight);
+// document
+//   .getElementById("lineHeight")
+//   .addEventListener("click", increaseLineHeight);
 
-document
-  .getElementById("saturationChange")
-  .addEventListener("click", saturationChange);
+// document
+//   .getElementById("saturationChange")
+//   .addEventListener("click", saturationChange);
 
-document.getElementById("legibleFonts").addEventListener("click", changeText);
+// document.getElementById("legibleFonts").addEventListener("click", changeText);
 
-document
-  .getElementById("cursorChanger")
-  .addEventListener("click", cursorChanger);
+// document
+//   .getElementById("cursorChanger")
+//   .addEventListener("click", cursorChanger);
 
-document
-  .getElementById("alignmentChange")
-  .addEventListener("click", changeAlignment);
+// document
+//   .getElementById("alignmentChange")
+//   .addEventListener("click", changeAlignment);
 
-document.getElementById("toolTip").addEventListener("click", toolTip);
+// document.getElementById("toolTip").addEventListener("click", toolTip);
 
-document.getElementById("close-widget").addEventListener("click", () => {
-  // print("Widget closed.");
-  const iframeHTML = parentDocument.querySelector(".iframe-page");
-  iframeHTML.style.display = "none";
-});
+// document.getElementById("close-widget").addEventListener("click", () => {
+//   // print("Widget closed.");
+//   const iframeHTML = parentDocument.querySelector(".iframe-page");
+//   iframeHTML.style.display = "none";
+// });
 
-document.getElementById("resetSettings").addEventListener("click", () => {
-  contrastChanger().resetMain();
-  highlightLink().resetStyles();
-  biggerText().resetStyles();
-  textSpacing().resetStyles();
-  changeText().resetStyles();
-  saturationChange().resetStyles();
-  increaseLineHeight().resetStyles();
-  hideImages().resetStyles();
-  toolTip().resetStyles();
-  changeAlignment().resetStyles();
-});
+// document.getElementById("resetSettings").addEventListener("click", () => {
+//   contrastChanger().resetMain();
+//   highlightLink().resetStyles();
+//   biggerText().resetStyles();
+//   textSpacing().resetStyles();
+//   changeText().resetStyles();
+//   saturationChange().resetStyles();
+//   increaseLineHeight().resetStyles();
+//   hideImages().resetStyles();
+//   toolTip().resetStyles();
+//   changeAlignment().resetStyles();
+// });
